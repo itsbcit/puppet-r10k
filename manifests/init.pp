86c957bb4ab4e464b6a44e2e7bc4c072e41b36e3
Jesse Weisner <jesse@weisner.ca>
2014-04-01 13:59:28 -0700

    install r10k gem

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class r10k (
+    'config_file'
+) {
+    $gemlist = [
+        "r10k",
+    ]
+
+    $package_deps = [
+        'rubygems',
+        'ruby-devel',
+    ]
+
+    package { $gemlist:
+        ensure   => installed,
+        provider => 'gem',
+        require  => Package[$package_deps],
+    }
+}
